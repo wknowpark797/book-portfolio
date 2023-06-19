e9e06cc9fe06f2354448830d883f5e1b5bfe15b4
unknown <wknowpark797@gmail.com>
2023-06-19 23:21:11 +0900

    location 서브 페이지 카카오 지도 api 기능 연동

--- js/location.js
+++ js/location.js
@@ -1,5 +1,9 @@
 const mapContainer = document.querySelector('#map');
+const infoWrap = document.querySelector('#infoWrap');
 const btns = document.querySelectorAll('.list-branch li');
+const btnTraffic = document.querySelector('.btn-traffic');
+let trafficToggle = false;
+let activeIndex = 0;
 
 const markerInfo = [
 	{
@@ -11,9 +15,9 @@ const markerInfo = [
 		},
 		phone: '02-6788-4211',
 		position: new kakao.maps.LatLng(37.531117, 126.917035),
-		imgSrc: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
-		imgSize: new kakao.maps.Size(64, 69),
-		imgOption: { offset: new kakao.maps.Point(27, 69) },
+		imgSrc: 'img/location-pin.png',
+		imgSize: new kakao.maps.Size(50, 50),
+		imgOption: { offset: new kakao.maps.Point(15, 50) },
 		button: btns[0],
 	},
 	{
@@ -25,9 +29,9 @@ const markerInfo = [
 		},
 		phone: '02-754-7338',
 		position: new kakao.maps.LatLng(37.552923, 126.981457),
-		imgSrc: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
-		imgSize: new kakao.maps.Size(64, 69),
-		imgOption: { offset: new kakao.maps.Point(27, 69) },
+		imgSrc: 'img/location-pin.png',
+		imgSize: new kakao.maps.Size(50, 50),
+		imgOption: { offset: new kakao.maps.Point(15, 50) },
 		button: btns[1],
 	},
 	{
@@ -39,9 +43,9 @@ const markerInfo = [
 		},
 		phone: '02-6002-3031',
 		position: new kakao.maps.LatLng(37.50999, 127.059986),
-		imgSrc: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
-		imgSize: new kakao.maps.Size(64, 69),
-		imgOption: { offset: new kakao.maps.Point(27, 69) },
+		imgSrc: 'img/location-pin.png',
+		imgSize: new kakao.maps.Size(50, 50),
+		imgOption: { offset: new kakao.maps.Point(15, 50) },
 		button: btns[2],
 	},
 	{
@@ -53,17 +57,28 @@ const markerInfo = [
 		},
 		phone: '02-535-4142',
 		position: new kakao.maps.LatLng(37.497669, 127.002837),
-		imgSrc: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
-		imgSize: new kakao.maps.Size(64, 69),
-		imgOption: { offset: new kakao.maps.Point(27, 69) },
+		imgSrc: 'img/location-pin.png',
+		imgSize: new kakao.maps.Size(50, 50),
+		imgOption: { offset: new kakao.maps.Point(15, 50) },
 		button: btns[3],
 	},
 ];
 
-// 지도 생성
+// 화면 로딩 시 지도 생성
 const mapOption = { center: markerInfo[0].position, level: 3 };
 const map = new kakao.maps.Map(mapContainer, mapOption);
+map.setZoomable(false); // 지도 확대, 축소 막기 (mousewheel)
+createDOM();
 
+// 지도 타입 컨트롤
+const mapTypeControl = new kakao.maps.MapTypeControl();
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 지도 확대, 축소 제어 줌 컨트롤
+const zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+// Marker 이미지 등록
 markerInfo.forEach((item, idx) => {
 	const markerImage = new kakao.maps.MarkerImage(item.imgSrc, item.imgSize, item.imgOption);
 	const marker = new kakao.maps.Marker({
@@ -73,9 +88,61 @@ markerInfo.forEach((item, idx) => {
 	marker.setMap(map);
 
 	item.button.addEventListener('click', (e) => {
-		for (const btn of btns) btn.classList.remove('on');
-		e.target.classList.add('on');
-
+		activeIndex = idx;
 		map.panTo(item.position);
+
+		for (const btn of btns) btn.classList.remove('on');
+		btns[activeIndex].classList.add('on');
+		createDOM();
 	});
+});
+
+// 도서관 정보 DOM 생성
+function createDOM() {
+	const tags = `
+		<h2>${markerInfo[activeIndex].title}</h2>
+		<ul>
+			<li>
+				<i class="fa-solid fa-location-dot"></i>
+				<p>${markerInfo[activeIndex].address}</p>
+			</li>
+			<li>
+				<i class="fa-solid fa-globe"></i>
+				<p>
+					<a href="${markerInfo[activeIndex].website.link}" target="_blank">
+						${markerInfo[activeIndex].website.title}
+					</a>
+				</p>
+			</li>
+			<li>
+				<i class="fa-solid fa-phone"></i>
+				<p>${markerInfo[activeIndex].phone}</p>
+			</li>
+		</ul>
+		<div class="sns-wrap">
+			<a href="#"><i class="fa-brands fa-instagram"></i></a>
+			<a href="#"><i class="fa-brands fa-youtube"></i></a>
+			<a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+		</div>
+	`;
+
+	infoWrap.innerHTML = tags;
+}
+
+// 지도 위치, Marker 가운데 고정
+window.addEventListener('resize', () => {
+	map.setCenter(markerInfo[activeIndex].position);
+});
+
+// 교통정보 표시
+btnTraffic.addEventListener('click', () => {
+	trafficToggle = !trafficToggle;
+
+	if (trafficToggle) {
+		map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+		btnTraffic.innerHTML = '교통정보 ON';
+	} else {
+		map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+		btnTraffic.innerHTML = '교통정보 OFF';
+	}
 });
