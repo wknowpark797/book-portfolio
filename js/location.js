10bbfb8325fdbd22696b10f39c6aa1b7ded0cc9e
wknowpark797 <wknowpark797@gmail.com>
2023-06-19 16:53:06 +0900

    location 서브 페이지 수정 중

--- js/location.js
+++ js/location.js
@@ -1,10 +1,15 @@
 const mapContainer = document.querySelector('#map');
 const btns = document.querySelectorAll('.list-branch li');
-const boxs = document.querySelectorAll('.library-info .inner-box');
 
 const markerInfo = [
 	{
 		title: '국회도서관',
+		address: '서울특별시 영등포구 의사당대로 1',
+		website: {
+			title: 'nanet.go.kr',
+			link: 'https://www.nanet.go.kr/main.do',
+		},
+		phone: '02-6788-4211',
 		position: new kakao.maps.LatLng(37.531117, 126.917035),
 		imgSrc: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
 		imgSize: new kakao.maps.Size(64, 69),
@@ -13,6 +18,12 @@ const markerInfo = [
 	},
 	{
 		title: '남산도서관',
+		address: '서울특별시 용산구 소월로 109',
+		website: {
+			title: 'nslib.sen.go.kr',
+			link: 'http://nslib.sen.go.kr/nslib_index.jsp',
+		},
+		phone: '02-754-7338',
 		position: new kakao.maps.LatLng(37.552923, 126.981457),
 		imgSrc: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
 		imgSize: new kakao.maps.Size(64, 69),
@@ -21,6 +32,12 @@ const markerInfo = [
 	},
 	{
 		title: '별마당 도서관',
+		address: '서울특별시 강남구 영동대로 513 스타필드 코엑스몰 B1',
+		website: {
+			title: 'starfield.co.kr',
+			link: 'https://www.starfield.co.kr/coexmall/starfieldLibrary/library.do',
+		},
+		phone: '02-6002-3031',
 		position: new kakao.maps.LatLng(37.50999, 127.059986),
 		imgSrc: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
 		imgSize: new kakao.maps.Size(64, 69),
@@ -29,6 +46,12 @@ const markerInfo = [
 	},
 	{
 		title: '국립중앙도서관',
+		address: '서울특별시 서초구 반포대로 201',
+		website: {
+			title: 'nl.go.kr',
+			link: 'https://www.nl.go.kr/',
+		},
+		phone: '02-535-4142',
 		position: new kakao.maps.LatLng(37.497669, 127.002837),
 		imgSrc: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
 		imgSize: new kakao.maps.Size(64, 69),
@@ -38,7 +61,7 @@ const markerInfo = [
 ];
 
 // 지도 생성
-const mapOption = { center: markerInfo[1].position, level: 3 };
+const mapOption = { center: markerInfo[0].position, level: 3 };
 const map = new kakao.maps.Map(mapContainer, mapOption);
 
 markerInfo.forEach((item, idx) => {
@@ -51,10 +74,7 @@ markerInfo.forEach((item, idx) => {
 
 	item.button.addEventListener('click', (e) => {
 		for (const btn of btns) btn.classList.remove('on');
-		for (const box of boxs) box.classList.remove('on');
-
 		e.target.classList.add('on');
-		boxs[idx].classList.add('on');
 
 		map.panTo(item.position);
 	});
