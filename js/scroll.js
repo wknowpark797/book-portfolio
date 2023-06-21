859800f5c5810b91974a3e5b71de63576d3e38fc
unknown <wknowpark797@gmail.com>
2023-06-21 20:21:33 +0900

    main gallery 영역 모션 추가

--- js/scroll.js
+++ js/scroll.js
@@ -2,25 +2,27 @@ const sections = document.querySelectorAll('.my-scroll');
 const btns = document.querySelectorAll('.scroll-navi li');
 
 // section의 높이값이 일괄적 100vh가 아닐 경우 고정 px로 지정
-const baseline = -100;
+const baseline = -200;
 
 window.addEventListener('scroll', () => {
-	sections.forEach((_, idx) => {
-		activation(idx);
-	});
+	activation();
 });
 
-function activation(idx) {
+function activation() {
 	const scroll = window.scrollY;
 
-	if (scroll >= sections[idx].offsetTop + baseline) {
-		for (const btn of btns) btn.classList.remove('on');
-		btns[idx].classList.add('on');
+	if (scroll <= 500) {
+		for (const el of sections) el.classList.remove('on');
+	}
 
-		if (!sections[idx].classList.contains('on')) {
+	sections.forEach((_, idx) => {
+		if (scroll >= sections[idx].offsetTop + baseline) {
+			for (const btn of btns) btn.classList.remove('on');
+			btns[idx].classList.add('on');
+
 			sections[idx].classList.add('on');
 		}
-	}
+	});
 }
 
 btns.forEach((btn, idx) => {
